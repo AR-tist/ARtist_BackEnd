75f036b0faf262bc4cb28ba1a207468a763d68ab
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-11-12 19:36:10 +0900

    Update main.py

--- python/source/main.py
+++ python/source/main.py
@@ -2,13 +2,22 @@
 from fastapi import FastAPI
 import uvicorn
 from routers import midi, root
-
+from fastapi.middleware.cors import CORSMiddleware
 
 ### FastAPI ###
 app = FastAPI()
 app.include_router(root.router)
 app.include_router(midi.router)
 
+
+### CORS ###
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 ### MAIN ###
 if __name__ == "__main__":

343543584ccc704915b78e99f559784449d11b7f
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-11-26 21:17:15 +0900

    Update connect.py

--- python/source/routers/multiplay/connect.py
+++ python/source/routers/multiplay/connect.py
@@ -12,7 +12,7 @@ from db.get_midi import get_midi
 import uuid
 
 async def connect(event, connected_clients, rooms):
-    if 'room_id' not in event:
+    if event['room_id'] == '':
         music_document = get_midi(event['filename'])
         client = Client(connectionID=event['connectionID'],
                         nickname=event['nickname'],

9a6962e35f65fbb7f10c88b2e41211d39f2dae2b
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-12-01 12:30:20 +0900

    Update imready.py

--- python/source/routers/multiplay/imready.py
+++ python/source/routers/multiplay/imready.py
@@ -1,5 +1,5 @@
 import time
-
+from datetime import datetime
 
 async def imready(event, connected_clients, rooms):
     room = rooms[event['room_id']]
@@ -16,9 +16,9 @@ async def imready(event, connected_clients, rooms):
             continue
     
     if all_loaded:
-        time.sleep(10)
+        now = int(datetime.now().timestamp() * 1000) # milliseconds since epoch
         for guest in room.guests:
             await connected_clients[guest].send_text(
-                str({'type': 'start', 'data': {}}).replace("'", '"')
+                str({'type': 'start', 'data': {now}}).replace("'", '"')
             )
         print(f'{event["connectionID"]} - {event["room_id"]} loading Complition and Start')

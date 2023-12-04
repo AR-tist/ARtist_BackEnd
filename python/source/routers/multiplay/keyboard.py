ebff01dcbe24799e5d1ee2ca2bc262d955842d4b
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-12-04 22:25:49 +0900

    Update keyboard.py

--- python/source/routers/multiplay/keyboard.py
+++ python/source/routers/multiplay/keyboard.py
@@ -4,7 +4,7 @@ async def keyDown(event, connected_clients, rooms):
     for guest in rooms[event['room_id']].guests:
         if guest != event['connectionID']:
             await connected_clients[guest].send_text(
-                str({'type': 'keyDown', 'data': event}).replace("'", '"')
+                str({'type': 'keyDown', 'data': event['data']}).replace("'", '"')
             )
 
 async def keyUp(event, connected_clients, rooms):
@@ -13,5 +13,5 @@ async def keyUp(event, connected_clients, rooms):
     for guest in rooms[event['room_id']].guests:
         if guest != event['connectionID']:
             await connected_clients[guest].send_text(
-               str({'type': 'keyDown', 'data': event}).replace("'", '"')
+               str({'type': 'keyDown', 'data': event['data']}).replace("'", '"')
             )

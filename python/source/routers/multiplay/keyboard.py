a40e4bf145153b6be89e1b8d9da04af691c02859
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-12-07 16:33:09 +0900

    Update keyboard.py

--- python/source/routers/multiplay/keyboard.py
+++ python/source/routers/multiplay/keyboard.py
@@ -4,7 +4,7 @@ async def keyDown(event, connected_clients, rooms):
     for guest in rooms[event['room_id']].guests:
         if guest != event['connectionID']:
             await connected_clients[guest].send_text(
-                str({'type': 'keyDown', 'data': event['data']}).replace("'", '"')
+                str({'type': 'keyDown', 'data': event['data'], 'user_id' : rooms[event['room_id']].guests[event['connectionID']].user_id}).replace("'", '"')
             )
 
 async def keyUp(event, connected_clients, rooms):
@@ -13,5 +13,5 @@ async def keyUp(event, connected_clients, rooms):
     for guest in rooms[event['room_id']].guests:
         if guest != event['connectionID']:
             await connected_clients[guest].send_text(
-               str({'type': 'keyDown', 'data': event['data']}).replace("'", '"')
+               str({'type': 'keyUp', 'data': event['data'], 'user_id' : rooms[event['room_id']].guests[event['connectionID']].user_id}).replace("'", '"')
             )

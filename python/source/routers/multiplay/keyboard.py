f774967a9c0618ae4d04d2eab245c8369f4a87be
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-12-07 18:32:39 +0900

    Update keyboard.py

--- python/source/routers/multiplay/keyboard.py
+++ python/source/routers/multiplay/keyboard.py
@@ -1,17 +1,20 @@
 async def keyDown(event, connected_clients, rooms):
     if event['room_id'] not in rooms:
         return
+    event['data']['user_id'] = rooms[event['room_id']].guests[event['connectionID']].user_id
     for guest in rooms[event['room_id']].guests:
         if guest != event['connectionID']:
             await connected_clients[guest].send_text(
-                str({'type': 'keyDown', 'data': event['data'], 'user_id' : rooms[event['room_id']].guests[event['connectionID']].user_id}).replace("'", '"')
+                str({'type': 'keyDown', 'data': event['data']}).replace("'", '"')
             )
 
 async def keyUp(event, connected_clients, rooms):
     if event['room_id'] not in rooms:
         return
+    event['data']['user_id'] = rooms[event['room_id']].guests[event['connectionID']].user_id
+
     for guest in rooms[event['room_id']].guests:
         if guest != event['connectionID']:
             await connected_clients[guest].send_text(
-               str({'type': 'keyUp', 'data': event['data'], 'user_id' : rooms[event['room_id']].guests[event['connectionID']].user_id}).replace("'", '"')
+               str({'type': 'keyUp', 'data': event['data']}).replace("'", '"')
             )

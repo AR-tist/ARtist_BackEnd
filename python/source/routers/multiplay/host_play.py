1a0cd9e1322ec017eccdbfff54d2c339cc3ce784
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-12-15 13:23:03 +0900

    Update host_play.py

--- python/source/routers/multiplay/host_play.py
+++ python/source/routers/multiplay/host_play.py
@@ -1,10 +1,19 @@
 import time
 
+from pymongo import MongoClient
+
 async def hostPlay(event, connected_clients, rooms):
     room = rooms[event['room_id']]
     for guest in room.guests:
         await connected_clients[guest].send_text(
             str({'type': 'areYouReady', 'data': {}}).replace("'", '"')
         )
+    client = MongoClient('mongodb://artist:1234@13.124.50.132:8484/')
+    db = client['artist']
+    collection = db['MidiFile']
+
+    # increment 1 to play_count field
+    collection.update_one({"filename": room.music_instance.filename}, {"$inc": {"views": 1}})
+
     print(f'{event["connectionID"]} - {event["nickname"]} command host Start in room {event["room_id"]}')
     

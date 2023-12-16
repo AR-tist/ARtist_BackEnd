b98401d319cfee44545f9cb11886c882d5e6a853
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-12-17 00:18:42 +0900

    Update get_midi.py

--- python/source/db/get_midi.py
+++ python/source/db/get_midi.py
@@ -11,7 +11,10 @@ def get_midi( filename ):
 
     download_url = f"/midi/download/{document['filename']}"
     delete_url = f"/midi/delete/{document['filename']}"
-    imgurl = f"/midi/img/{document['imgurl']}"
+    if document['imgurl'] == "":
+        imgurl = ""
+    else:
+        imgurl = f"/midi/downlaod/{document['imgurl']}"
     document['download_url'] = download_url
     document['delete_url'] = delete_url
     document['imgurl'] = imgurl
